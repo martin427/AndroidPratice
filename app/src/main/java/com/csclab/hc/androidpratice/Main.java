3e61f5b0a4cc83d7a2be862f3d8ea9100ff40377
martin427 <martintw.kuo@gmail.com>
2016-05-31 20:57:23 +0800

    done

---
+++ app/src/main/java/com/csclab/hc/androidpratice/Main.java
@@ -0,0 +1,10 @@
+package com.csclab.hc.androidpratice;
+
+/**
+ * Created by NTHUCS on 2016/5/31.
+ */
+public class Main {
+    public static void main(String[] args){
+        Server obj = new Server();
+    }
+}
---
+++ app/src/main/res/javaserver/src/main/java/com/example/Main.java
@@ -0,0 +1,8 @@
+package javaserver.src.main.java.com.example;
+
+
+public class Main {
+    public static void main(String[] args){
+        Server obj = new Server();
+    }
+}
